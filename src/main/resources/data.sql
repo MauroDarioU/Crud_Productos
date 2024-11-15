82b988008381a24cb7b65e9367cd481c5ef2e39a
Mauro <unate_mauro@hotmail.com>
2024-11-15 19:02:36 -0300

    -m

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- carga de datos en la tabla productos--
 
-INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Laptop', 'Laptop de alta gama', 1500.0, 10);
-INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Teléfono', 'Teléfono inteligente', 500.0, 25);
-INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Tablet', 'Tablet para toda la familia', 300.0, 15);
+INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Laptop', 'Laptop de alta gama', 1500.0, 10, NOW(),NOW());
+INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Teléfono', 'Teléfono inteligente', 500.0, 25, NOW(),NOW());
+INSERT INTO crud_producto (nombre, descripcion, precio, stock, created_at, updated_at) VALUES ('Tablet', 'Tablet para toda la familia', 300.0, 15, NOW(),NOW());
